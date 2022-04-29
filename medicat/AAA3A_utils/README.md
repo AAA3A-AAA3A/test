081b2f94ddb1b6d56f08a3a4f3f060ceee47ebbc
AAA3A-AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2022-04-29 14:39:49 +0200

    Add files via upload

---
+++ medicat/AAA3A_utils/README.md
@@ -0,0 +1,10 @@
+## My utils
+
+Hello there! If you're contributing or taking a look, everything in this folder
+is synced from a master repo at https://github.com/AAA3A-AAA3A/AAA3A_utils by GitHub Actions -
+so it's probably best to look/edit there.
+
+---
+
+Last sync at: 2022-04-06 11:49:34 UTC
+Commit: [`1c87c9b82bec01235c4952c346731d6bbf40cd35`](https://github.com/AAA3A-AAA3A/AAA3A_utils/commit/1c87c9b82bec01235c4952c346731d6bbf40cd35)
---
+++ medicat/medicat.py
@@ -0,0 +1,66 @@
+﻿from .AAA3A_utils.cogsutils import CogsUtils, Menu  # isort:skip
+from redbot.core import commands  # isort:skip
+import discord  # isort:skip
+import typing  # isort:skip
+
+import aiohttp
+
+from redbot import VersionInfo
+from redbot.core import Config
+
+# Credits:
+# Thanks to @epic guy on Discord for the basic syntax (command groups, commands) and also commands (await ctx.send, await ctx.author.send, await ctx.message.delete())!
+# Thanks to the developers of the cogs I added features to as it taught me how to make a cog! (Chessgame by WildStriker, Captcha by Kreusada, Speak by Epic guy and Rommer by Dav)
+# Thanks to all the people who helped me with some commands in the #coding channel of the redbot support server!
+
+MEDICAT_GUILD = 829469886681972816
+VENTOY_UPDATES_CHANNEL = 831224763162165278
+MODERATORS_ROLE = 829472084454670346
+DEVELOPER_ROLE = 883612487881195520
+MEMBERS_ROLE = 829538904720932884
+
+def _(untranslated: str):
+    return untranslated
+
+class Medicat(commands.Cog):
+    """This cog will only work on x server and therefore cannot be used by the general public!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+        self.config: Config = Config.get_conf(
+            self,
+            identifier=953864285308,
+            force_registration=True,
+        )
+        self.medicat_global = {
+            "last_ventoy_version": "1.0.73",
+        }
+        self.config.register_guild(**self.medicat_global)
+
+        self.cogsutils = CogsUtils(cog=self)
+        self.cogsutils._setup()
+        
+        self.cogsutils.create_loop(function=self.ventoy_updates, name="Ventoy Updates Loop", hours=1)
+
+    async def ventoy_updates(self):
+        guild = self.bot.get_guild(MEDICAT_GUILD)
+        channel = guild.get_channel(VENTOY_UPDATES_CHANNEL)
+        last_ventoy_version_str = str(self.config.last_ventoy_version())
+        last_ventoy_version = VersionInfo.from_str(last_ventoy_version_str)
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://api.github.com/repos/ventoy/Ventoy/git/refs/tags", timeout=3) as r:
+                ventoy_tags = await r.json()
+        ventoy_version_str = str(ventoy_tags[len(ventoy_tags) - 1]["ref"].replace("refs/tags/", ""))
+        ventoy_version = VersionInfo.from_str(ventoy_version_str)
+        
+        if last_ventoy_version >= ventoy_version:
+            return
+        
+        await self.config.last_ventoy_version.set(ventoy_version)
+
+        message = f"Ventoy {ventoy_version} has been released!\nhttps://ventoy.net/en/index.html"
+        hook = await CogsUtils(bot=self.bot).get_hook(channel)
+        message = await hook.send(content=message, username="Ventoy Updates", avatar_url="https://ventoy.net/static/img/ventoy.png?v=1")
+        await message.publish()
